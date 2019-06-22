585cc8ce285671c7c4f88ecdc20ac81a9f1e2f68
Dominique De León <dominique.a.deleon@gmail.com>
2019-06-22 08:59:37 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -14,10 +14,10 @@ def third_challenge
    }, 
    :capulet => {
       :patriarch => {
-        name: "Lord Capulet", age: "53"
+        name: "Lord Capulet", age: "50"
         },
       :matriarch => {
-        #your key/value pairs here
+        name: "Lady Capulet", age: "5q"
         },
       :heroine => {
         #your key/value pairs here
